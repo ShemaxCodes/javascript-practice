c7f6e274ec08ba08f03f0b81b91dc92f080ab818
Jashema Panapa <jcpanapa@gmail.com>
2021-10-05 16:56:55 -0400

    modified:   varletconst.js

--- varletconst.js
+++ varletconst.js
@@ -7,3 +7,59 @@ let age = 33
 const person = {name, age}
 person.name = "Toa"
 console.log(person)
+
+// Conditionals 
+
+// In Ruby, if statements were like so:
+// if condition
+//statement
+//end 
+// In JS
+//if (condition) {
+  //  statement
+//}
+let num = 4 
+
+//if (true) {
+    //console.log("Hello")
+//} else if (num === 3) {
+    //console.log("Yes!")
+// else { console.log("Num is none of these") 
+//}
+
+// Falsey and Truthy
+
+// > null
+//null
+//> !!5
+//true
+//> !!"3"
+//true
+//> !!NaN
+//false
+//> !!{}
+//true
+//> !!""
+//false
+//2 == 2
+//true
+//> 2 === 2
+///true
+//> "2" == 2
+//true
+//> "2" === 2
+//false
+let num = 4 
+
+switch(num) {
+    case 1:
+        console.log("Num is 1");
+
+    case 2:
+        console.log("Num is the same as Gizmo's age");
+    default:
+        console.log("It is none of these")
+}
+
+let students = ["Gizmo", "Toa", "Shema", "Reina", "TJ"]
+students.splice(2,2)
